265c642564d8409fa7688dd08efc3cf745b9a963
paulJupilliat <jupilliatp@gmail.com>
2023-02-08 13:22:04 +0100

    modif des objectifs du readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PHP_QUIZ
 travail colaboratif avec Ben
+
+
+idee amélioration:
+ajout de niveau de difficulté => question plus difficile
+ajout de temps limite => question plus rapide
+ajoout de l'attribut neg_point => question a point négatif
--- html/resultat.php
+++ html/resultat.php
@@ -4,6 +4,12 @@ if (isset($_SESSION['pseudo'])) {
     echo 'Bonjour ' . $_SESSION['pseudo'] . ' voici vos resultat du quiz !';
 }
 $theme = $_GET['theme'];
+// je requette la bonne réponse de ma question
+// si la bonne réponse est égale à la réponse de l'utilisateur
+// alors je lui ajoute un point
+// sinon je lui enlève les points si c'est a points négatifs
+// je lui affiche son score
+
 
 ?>
 <!Doctype html>
